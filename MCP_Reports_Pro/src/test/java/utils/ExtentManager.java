f3bc989b6bce2eadc63bb1a523de45c4524d762e
HarshAkhand05 <harshakhand998@gmail.com>
2026-04-07 12:43:32 +0530

    added again target folder

--- MCP_Reports_Pro/src/test/java/utils/ExtentManager.java
+++ MCP_Reports_Pro/src/test/java/utils/ExtentManager.java
@@ -10,7 +10,7 @@ public class ExtentManager {
     public static ExtentReports getInstance() {
         if (extent == null) {
             ExtentSparkReporter reporter =
-                    new ExtentSparkReporter("Reportfolder/ExtentReport.html");
+                    new ExtentSparkReporter("target/ExtentReport.html");
 
             reporter.config().setReportName("DemoQA Login Report");
             reporter.config().setDocumentTitle("Login Test Results");
